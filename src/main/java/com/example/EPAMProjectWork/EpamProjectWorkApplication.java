789ade2c6f77cc92c3d8c0440f4b4f46ee6454fa
Malina-ept <75109750+Malina-ept@users.noreply.github.com>
2021-08-13 20:51:59 +0300

    Создан проект, добавлен тест viewUpcomingEvents

---
+++ src/main/java/com/example/EPAMProjectWork/EpamProjectWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.EPAMProjectWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpamProjectWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpamProjectWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EPAMProjectWork/EpamProjectWorkApplicationTests.java
@@ -0,0 +1,78 @@
+package com.example.EPAMProjectWork;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class EpamProjectWorkApplicationTests {
+
+	private Logger logger = LogManager.getLogger(EpamProjectWorkApplicationTests.class);
+	protected static WebDriver driver;
+
+	@Before
+	public void StartUp(){
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		logger.info("Драйвер поднят");
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	}
+
+	@After
+	public void End(){
+		if (driver!=null)
+			driver.quit();
+	}
+
+	@Test
+	public void viewUpcomingEvents() {
+		logger.info("Тест стaрт");
+		driver.get("https://events.epam.com/");
+		logger.info("Сайт открыт");
+		driver.manage().window().maximize();
+		logger.info("Открыто окно браузера на полный экран");
+
+		logger.info("Переходим на вкладку events");
+		driver.findElement(By.xpath("//li[@class =  'nav-item events-icon']")).click();
+		logger.info("Открыта вкладка events");
+
+		logger.info("Найдем нажатую кнопку Upcoming events");
+		WebElement actionButton = (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class = 'evnt-tab-link nav-link active']//span[text()='Upcoming events']")));
+		String actualButtonText = driver.findElement(By.xpath("//a[@class = 'evnt-tab-link nav-link active']//span[text()='Upcoming events']")).getText();
+//		String actualButtonText =	driver.findElement(By.xpath("//a[@class = 'evnt-tab-link nav-link active']")).getText();
+		assertTrue("Нажатая кнопка не соответствует Upcoming events ",actualButtonText.contains("UPCOMING EVENTS"));
+		logger.info("Активная кнопка: Upcoming events");
+
+		logger.info("Возьмем количество карточек на кнопке Upcoming events");
+		String countOfCardsOnButton = driver.findElement(By.xpath("//a[@class = 'evnt-tab-link nav-link active']//span[3]")).getText();
+		logger.info("Возьмем количество карточек на странице");
+		WebElement countOfCardsOnPage = driver.findElement(By.xpath("//div[@class = 'evnt-events-column cell-3']"));
+
+		Object[] array = {driver.findElement(By.xpath("//div[@class = 'evnt-events-column cell-3']"))};
+		System.out.println(array.length);
+
+		logger.info("и сравним их количество");
+		assertEquals( "Кол-во на кнопке не совпадает с кол-вом карточек на странице", countOfCardsOnButton, countOfCardsOnPage.getText());
+
+
+	}
+
+
+
+
+}
